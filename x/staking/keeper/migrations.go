afae72eba78c1d48ebd56b9398b733622e580717
evan-forbes <evan.samuel.forbes@gmail.com>
2022-01-09 15:01:38 -0600

    add unused migration

--- x/staking/keeper/migrations.go
+++ x/staking/keeper/migrations.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v043 "github.com/cosmos/cosmos-sdk/x/staking/legacy/v043"
+	v045 "github.com/cosmos/cosmos-sdk/x/staking/migrations/v045"
 )
 
 // Migrator is a struct for handling in-place store migrations.
